9d4da5b4c01b768e7fc8ae2620d7670a5071bece
Dayan Walter <dayanwalter@gmail.com>
2025-06-16 14:09:43 -0600

    fix: add asChild to button

--- components/intro-section.tsx
+++ components/intro-section.tsx
@@ -3,81 +3,80 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 
 export default function Intro() {
-  const githubLink = "https://github.com/dayanwalter";
-  const linkedinLink = "https://www.linkedin.com/in/dayanwalter/";
-  const email = "dayanwalter@gmail.com";
+	const githubLink = "https://github.com/dayanwalter";
+	const linkedinLink = "https://www.linkedin.com/in/dayanwalter/";
+	const email = "dayanwalter@gmail.com";
 
-  const socialLinks = [
-    {
-      href: githubLink,
-      label: "github button",
-      icon: <Github />,
-    },
-    {
-      href: linkedinLink,
-      label: "linkedIn button",
-      icon: <Linkedin />,
-    },
-    {
-      href: `mailto:${email}`,
-      label: "email button",
-      icon: <Mail />,
-    },
-  ];
+	const socialLinks = [
+		{
+			href: githubLink,
+			label: "github button",
+			icon: <Github />,
+		},
+		{
+			href: linkedinLink,
+			label: "linkedIn button",
+			icon: <Linkedin />,
+		},
+		{
+			href: `mailto:${email}`,
+			label: "email button",
+			icon: <Mail />,
+		},
+	];
 
-  return (
-    <>
-      {/* Section */}
-      <section
-        id="intro"
-        className="grid justify-center py-20 md:h-screen md:py-24"
-      >
-        {/* Container */}
-        <div className="grid items-center px-4 py-6 md:max-w-7xl md:border-l md:px-8">
-          {/* Content */}
-          <div className="grid gap-8 md:gap-12">
-            {/* Heading and supporting text */}
-            <div className="grid gap-6 md:gap-10">
-              {/* Heading */}
-              <h1 className="text-5xl leading-10 font-semibold md:text-7xl md:leading-[1.15]">
-                Amazing Apps, Clean Code.
-              </h1>
-              {/* Supporting text */}
-              <p className="md:max-w-3xl md:text-xl">
-                I am Dayan - father, husband and freelance software developer
-                specializing in Next.js, React.js, Nest.js and PostgreSQL.
-              </p>
+	return (
+		<>
+			{/* Section */}
+			<section
+				id="intro"
+				className="grid justify-center py-20 md:h-screen md:py-24"
+			>
+				{/* Container */}
+				<div className="grid items-center px-4 py-6 md:max-w-7xl md:border-l md:px-8">
+					{/* Content */}
+					<div className="grid gap-8 md:gap-12">
+						{/* Heading and supporting text */}
+						<div className="grid gap-6 md:gap-10">
+							{/* Heading */}
+							<h1 className="text-5xl leading-10 font-semibold md:text-7xl md:leading-[1.15]">
+								Amazing Apps, Clean Code.
+							</h1>
+							{/* Supporting text */}
+							<p className="md:max-w-3xl md:text-xl">
+								I am Dayan - father, husband and freelance software developer
+								specializing in Next.js, React.js, Nest.js and PostgreSQL.
+							</p>
 
-              <p className="md:max-w-3xl md:text-xl">
-                From Lego to Minecraft and now to functional React components –
-                the play has never stopped.
-              </p>
-              <p className="md:max-w-3xl md:text-xl">
-                I develop efficient, modular solutions for complex applications,
-                combining creativity and precision to deliver outstanding
-                results.
-              </p>
-              {/* Links */}
-              <div className="flex gap-3">
-                {socialLinks.map(({ href, label, icon }) => (
-                  <Button
-                    asChild
-                    size="icon"
-                    role="button"
-                    aria-label={label}
-                    className="cursor-pointer"
-                    key={label}
-                  >
-                    <Link href={href} target="_blank" rel="noopener noreferrer">
-                      {icon}
-                    </Link>
-                  </Button>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+							<p className="md:max-w-3xl md:text-xl">
+								From Lego to Minecraft and now to functional React components –
+								the play has never stopped.
+							</p>
+							<p className="md:max-w-3xl md:text-xl">
+								I develop efficient, modular solutions for complex applications,
+								combining creativity and precision to deliver outstanding
+								results.
+							</p>
+							{/* Links */}
+							<div className="flex gap-3">
+								{socialLinks.map(({ href, label, icon }) => (
+									<Button
+										asChild
+										size="icon"
+										aria-label={label}
+										className="cursor-pointer"
+										key={label}
+									>
+										<Link href={href} target="_blank" rel="noopener noreferrer">
+											{icon}
+										</Link>
+									</Button>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
--- components/project-section.tsx
+++ components/project-section.tsx
@@ -3,89 +3,89 @@ import { PROJECTS } from "./constants";
 import Link from "next/link";
 
 export default function Projects() {
-  return (
-    <>
-      {/* Section */}
-      <section id="projects" className="grid justify-center py-20 md:py-24">
-        {/* Container */}
-        <div className="grid gap-16 border-l px-4 md:max-w-7xl md:gap-20 md:px-8">
-          {/* Content */}
-          <div>
-            {/* Heading */}
-            <div className="grid gap-1 md:gap-2">
-              <h2 className="text-3xl font-semibold md:text-4xl">Projects</h2>
-              <p className="">These are my newest projects</p>
-            </div>
-          </div>
+	return (
+		<>
+			{/* Section */}
+			<section id="projects" className="grid justify-center py-20 md:py-24">
+				{/* Container */}
+				<div className="grid gap-16 border-l px-4 md:max-w-7xl md:gap-20 md:px-8">
+					{/* Content */}
+					<div>
+						{/* Heading */}
+						<div className="grid gap-1 md:gap-2">
+							<h2 className="text-3xl font-semibold md:text-4xl">Projects</h2>
+							<p className="">These are my newest projects</p>
+						</div>
+					</div>
 
-          {/* Content */}
-          <ul className="grid gap-48">
-            {PROJECTS.map((project) => (
-              <li
-                key={project?.id}
-                className="flex flex-col gap-5 md:flex-row md:gap-10"
-              >
-                <Link
-                  aria-label={`Link to ${project?.title}`}
-                  target="_blank"
-                  href={project?.websiteLink}
-                >
-                  <video
-                    className="w-max rounded-md border shadow-sm transition-transform hover:-translate-y-1 hover:shadow-md md:max-w-xl"
-                    autoPlay
-                    muted
-                    loop
-                  >
-                    <source src={project?.video} type="video/webm" />
-                    Your browser does not support the video tag.
-                  </video>
-                </Link>
+					{/* Content */}
+					<ul className="grid gap-48">
+						{PROJECTS.map((project) => (
+							<li
+								key={project?.id}
+								className="flex flex-col gap-5 md:flex-row md:gap-10"
+							>
+								<Link
+									aria-label={`Link to ${project?.title}`}
+									target="_blank"
+									href={project?.websiteLink}
+								>
+									<video
+										className="w-max rounded-md border shadow-sm transition-transform hover:-translate-y-1 hover:shadow-md md:max-w-xl"
+										autoPlay
+										muted
+										loop
+									>
+										<source src={project?.video} type="video/webm" />
+										Your browser does not support the video tag.
+									</video>
+								</Link>
 
-                {/* Heading and text */}
-                <div className="flex flex-col gap-5">
-                  {/* Heading and tech */}
-                  <div className="">
-                    <Link
-                      href={project?.websiteLink}
-                      aria-label="link to website"
-                    >
-                      <p className="text-xl font-semibold">{project?.title}</p>
-                    </Link>
+								{/* Heading and text */}
+								<div className="flex flex-col gap-5">
+									{/* Heading and tech */}
+									<div className="">
+										<Link
+											href={project?.websiteLink}
+											aria-label="link to website"
+										>
+											<p className="text-xl font-semibold">{project?.title}</p>
+										</Link>
 
-                    {/* Tech */}
-                    <p className="text-sm italic">{project?.tech}</p>
-                  </div>
+										{/* Tech */}
+										<p className="text-sm italic">{project?.tech}</p>
+									</div>
 
-                  {/* Description */}
-                  <p className="">{project?.descriptionEN}</p>
+									{/* Description */}
+									<p className="">{project?.descriptionEN}</p>
 
-                  {/* Links */}
-                  <div className="flex gap-5">
-                    <Button size="sm">
-                      <Link
-                        href={project?.codeLink}
-                        aria-label="Link to code"
-                        target="_blank"
-                      >
-                        View Code
-                      </Link>
-                    </Button>
-                    <Button size="sm">
-                      <Link
-                        href={project?.websiteLink}
-                        aria-label="Link to website"
-                        target="_blank"
-                      >
-                        Visit website
-                      </Link>
-                    </Button>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-    </>
-  );
+									{/* Links */}
+									<div className="flex gap-5">
+										<Button size="sm" asChild>
+											<Link
+												href={project?.codeLink}
+												aria-label="Link to code"
+												target="_blank"
+											>
+												View Code
+											</Link>
+										</Button>
+										<Button size="sm" asChild>
+											<Link
+												href={project?.websiteLink}
+												aria-label="Link to website"
+												target="_blank"
+											>
+												Visit website
+											</Link>
+										</Button>
+									</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			</section>
+		</>
+	);
 }
